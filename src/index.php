7e24ec750f5ab390e3582fa8feb391e331db8612
Alexander Dreweke <alexander@dreweke.net>
2023-07-14 17:15:34 +0200

    make script more robust and better suited for docker

--- src/index.php
+++ src/index.php
@@ -1,25 +1,4 @@
 <?php
-const WRITE_SQL_FILE = getenv('WRITE_SQL_FILE') ?: true;
-const DATABASE_INI = getenv('DATABASE_INI') ?: '../database.ini';
-const SQL_FOLDER = getenv('SQL_FOLDER') ?: '../sql';
-
-function connect_db($file) {
-    $db = parse_ini_file($file);
-    $connection = sprintf('pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s;sslmode=%s',
-        $db['host'],
-        $db['port'],
-        $db['dbname'],
-        $db['user'],
-        $db['password'],
-        $db['sslmode']);
-
-    $pdo = new \PDO($connection);
-    $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-    return $pdo;
-}
-
-$pdo = connect_db(DATABASE_INI);
-
 $raw = file_get_contents('php://input');
 if (!$raw) {
     http_response_code(400); // Bad Request (no payload)
@@ -28,7 +7,7 @@ if (!$raw) {
 
 $json = json_decode($raw, true);
 if ($json == NULL) {
-    http_response_code(400); // Bad Request (invalid json)
+    http_response_code(422); // Bad Request (invalid json)
     return;
 }
 
@@ -42,22 +21,9 @@ $statement  = 'INSERT INTO measurement ';
 $statement .= '(time, sensor_id, firmware, bmp_pressure, bmp_temperature, heca_humidity, heca_temperature, pm_10, pm_25, sht_humidity, sht_temperature, wifi_signal) ';
 $statement .= 'VALUES(:time, :sensor_id, :firmware, :bmp_pressure, :bmp_temperature, :heca_humidity, :heca_temperature, :pm_10, :pm_25, :sht_humidity, :sht_temperature, :wifi_signal)';
 $statement .= 'ON CONFLICT (time, sensor_id) DO NOTHING';
-$insert = $pdo->prepare($statement);
-$insert->bindValue(':time',             $now,                                                      PDO::PARAM_STR);
-$insert->bindValue(':sensor_id',        $json['esp8266id'],                                        PDO::PARAM_INT);
-$insert->bindValue(':firmware',         $json['software_version'],                                 PDO::PARAM_STR);
-$insert->bindValue(':bmp_pressure',     floatval($sensor_data_values['BMP_pressure'] ?? -99),      PDO::PARAM_INT);
-$insert->bindValue(':bmp_temperature',  floatval($sensor_data_values['BMP_temperature'] ?? -99),   PDO::PARAM_INT);
-$insert->bindValue(':heca_humidity',    floatval($sensor_data_values['HECA_humidity'] ?? -1),      PDO::PARAM_INT);
-$insert->bindValue(':heca_temperature', floatval($sensor_data_values['HECA_temperature'] ?? -99),  PDO::PARAM_INT);
-$insert->bindValue(':pm_10',            floatval($sensor_data_values['SDS_P1'] ?? -1),             PDO::PARAM_INT);
-$insert->bindValue(':pm_25',            floatval($sensor_data_values['SDS_P2'] ?? -1),             PDO::PARAM_INT);
-$insert->bindValue(':sht_humidity',     floatval($sensor_data_values['SHT3X_humidity'] ?? -1),     PDO::PARAM_INT);
-$insert->bindValue(':sht_temperature',  floatval($sensor_data_values['SHT3X_temperature'] ?? -99), PDO::PARAM_INT);
-$insert->bindValue(':wifi_signal',      floatval($sensor_data_values['signal'] ?? -1),             PDO::PARAM_INT);
-
-if (WRITE_SQL_FILE) {
-    $file = SQL_FOLDER . '/sensor-' . date('Y-m-d') . '.sql';
+if (getenv('WRITE_SQL_FILE')) {
+    $sql_folder = getenv('SQL_FOLDER') ?: '../sql';
+    $file = $sql_folder . '/sensor-' . date('Y-m-d') . '.sql';
     $fd = fopen($file, 'a');
     if (!$fd) {
         http_response_code(500); // Internal Server Error
@@ -87,6 +53,46 @@ if (WRITE_SQL_FILE) {
         return;
     }
 }
+
+$pdo = null;
+if (getenv('DB_CONNECTION')) {
+    $pdo = new \PDO(getenv('DB_CONNECTION'));
+} else {
+    $file='../database.ini';
+    if (!file_exists($file)) {
+        http_response_code(500); // Internal Server Error
+        return;
+    }
+
+    $db = parse_ini_file($file);
+    if (!$db) {
+        http_response_code(500); // Internal Server Error
+        return;
+    }
+    $connection = sprintf('pgsql:host=%s;port=%d;dbname=%s;user=%s;password=%s;sslmode=%s',
+        $db['host'],
+        $db['port'],
+        $db['dbname'],
+        $db['user'],
+        $db['password'],
+        $db['sslmode']);
+    $pdo = new \PDO($connection);
+}
+$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+
+$insert = $pdo->prepare($statement);
+$insert->bindValue(':time',             $now,                                                      PDO::PARAM_STR);
+$insert->bindValue(':sensor_id',        $json['esp8266id'],                                        PDO::PARAM_INT);
+$insert->bindValue(':firmware',         $json['software_version'],                                 PDO::PARAM_STR);
+$insert->bindValue(':bmp_pressure',     floatval($sensor_data_values['BMP_pressure'] ?? -99),      PDO::PARAM_INT);
+$insert->bindValue(':bmp_temperature',  floatval($sensor_data_values['BMP_temperature'] ?? -99),   PDO::PARAM_INT);
+$insert->bindValue(':heca_humidity',    floatval($sensor_data_values['HECA_humidity'] ?? -1),      PDO::PARAM_INT);
+$insert->bindValue(':heca_temperature', floatval($sensor_data_values['HECA_temperature'] ?? -99),  PDO::PARAM_INT);
+$insert->bindValue(':pm_10',            floatval($sensor_data_values['SDS_P1'] ?? -1),             PDO::PARAM_INT);
+$insert->bindValue(':pm_25',            floatval($sensor_data_values['SDS_P2'] ?? -1),             PDO::PARAM_INT);
+$insert->bindValue(':sht_humidity',     floatval($sensor_data_values['SHT3X_humidity'] ?? -1),     PDO::PARAM_INT);
+$insert->bindValue(':sht_temperature',  floatval($sensor_data_values['SHT3X_temperature'] ?? -99), PDO::PARAM_INT);
+$insert->bindValue(':wifi_signal',      floatval($sensor_data_values['signal'] ?? -1),             PDO::PARAM_INT);
 $insert->execute();
 
 http_response_code(201); // Created
